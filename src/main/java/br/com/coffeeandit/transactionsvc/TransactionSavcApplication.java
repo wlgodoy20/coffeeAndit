31f7bd2b0e53d525c59f360443f8e312291d529e
Welson Godoi <wlgodoy20@gmail.com>
2022-03-31 20:59:19 -0300

    commit inicial

---
+++ src/main/java/br/com/coffeeandit/transactionsvc/TransactionSavcApplication.java
@@ -0,0 +1,13 @@
+package br.com.coffeeandit.transactionsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSavcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionSavcApplication.class, args);
+    }
+
+}
